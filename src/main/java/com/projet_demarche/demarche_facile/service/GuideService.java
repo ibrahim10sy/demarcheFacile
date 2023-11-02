2ba9c499bc7c37abc4e81c9aac16946c02acca97
ibrahim10sy <ibrahim10sy@gmail.com>
2023-11-02 13:57:25 +0000

    commit

--- src/main/java/com/projet_demarche/demarche_facile/service/GuideService.java
+++ src/main/java/com/projet_demarche/demarche_facile/service/GuideService.java
@@ -1,6 +1,8 @@
 package com.projet_demarche.demarche_facile.service;
 
+import com.projet_demarche.demarche_facile.model.Admin;
 import com.projet_demarche.demarche_facile.model.Guide;
+import com.projet_demarche.demarche_facile.repository.AdminRepository;
 import com.projet_demarche.demarche_facile.repository.GuideRepository;
 import jakarta.persistence.EntityExistsException;
 import jakarta.persistence.EntityNotFoundException;
@@ -18,81 +20,88 @@ public class GuideService {
 
     @Autowired
     public GuideRepository guideRepository;
+    @Autowired
+    public AdminRepository adminRepository;
 
     //methode ajout
     public Guide createGuide(Guide guide, MultipartFile multipartFileImage, MultipartFile multipartFileAudio) throws Exception {
-        if(guideRepository.findByLibelle(guide.getLibelle()) == null){
-            //ajout de l'image
-            if(multipartFileImage != null){
-                String location = "C:\\xampp\\htdocs\\demarche";
-                try{
-                    Path rootlocation = Paths.get(location);
-                    if(!Files.exists(rootlocation)){
-                        Files.createDirectories(rootlocation);
-                        Files.copy(multipartFileImage.getInputStream(),
-                                rootlocation.resolve(multipartFileImage.getOriginalFilename()));
-                        guide.setImage("http://localhost/demarche/"
-                                +multipartFileImage.getOriginalFilename());
-                    }else{
-                        try {
-                            String nom = location+"\\"+multipartFileImage.getOriginalFilename();
-                            Path name = Paths.get(nom);
-                            if(!Files.exists(name)){
-                                Files.copy(multipartFileImage.getInputStream(),
-                                        rootlocation.resolve(multipartFileImage.getOriginalFilename()));
-                                guide.setImage("http://localhost/demarche/"
-                                        +multipartFileImage.getOriginalFilename());
-                            }else{
-                                Files.delete(name);
-                                Files.copy(multipartFileImage.getInputStream(),rootlocation.resolve(multipartFileImage.getOriginalFilename()));
-                                guide.setImage("http://localhost/demarche/"
-                                        +multipartFileImage.getOriginalFilename());
+        Admin admin = adminRepository.findByIdAdmin(guide.getAdmin().getIdAdmin());
+        if(admin == null) {
+            throw new EntityExistsException("Compte non trouvé");
+        }else {
+            if (guideRepository.findByLibelle(guide.getLibelle()) == null) {
+                //ajout de l'image
+                if (multipartFileImage != null) {
+                    String location = "C:\\xampp\\htdocs\\demarche";
+                    try {
+                        Path rootlocation = Paths.get(location);
+                        if (!Files.exists(rootlocation)) {
+                            Files.createDirectories(rootlocation);
+                            Files.copy(multipartFileImage.getInputStream(),
+                                    rootlocation.resolve(multipartFileImage.getOriginalFilename()));
+                            guide.setImage("http://localhost/demarche/"
+                                    + multipartFileImage.getOriginalFilename());
+                        } else {
+                            try {
+                                String nom = location + "\\" + multipartFileImage.getOriginalFilename();
+                                Path name = Paths.get(nom);
+                                if (!Files.exists(name)) {
+                                    Files.copy(multipartFileImage.getInputStream(),
+                                            rootlocation.resolve(multipartFileImage.getOriginalFilename()));
+                                    guide.setImage("http://localhost/demarche/"
+                                            + multipartFileImage.getOriginalFilename());
+                                } else {
+                                    Files.delete(name);
+                                    Files.copy(multipartFileImage.getInputStream(), rootlocation.resolve(multipartFileImage.getOriginalFilename()));
+                                    guide.setImage("http://localhost/demarche/"
+                                            + multipartFileImage.getOriginalFilename());
+                                }
+                            } catch (Exception e) {
+                                throw new Exception("Impossible de télécharger l\'image");
                             }
-                        }catch (Exception e){
-                            throw new Exception("Impossible de télécharger l\'image");
                         }
+                    } catch (Exception e) {
+                        throw new Exception(e.getMessage());
                     }
-                } catch (Exception e){
-                    throw new Exception(e.getMessage());
                 }
-            }
-            //ajout de l'audio
-            if(multipartFileAudio != null){
-                String location = "C:\\xampp\\htdocs\\audio_demarche";
-                try{
-                    Path rootlocation = Paths.get(location);
-                    if(!Files.exists(rootlocation)){
-                        Files.createDirectories(rootlocation);
-                        Files.copy(multipartFileAudio.getInputStream(),
-                                rootlocation.resolve(multipartFileAudio.getOriginalFilename()));
-                        guide.setAudio("http://localhost/audio_demarche/"
-                                +multipartFileAudio.getOriginalFilename());
-                    }else{
-                        try {
-                            String nom = location+"\\"+multipartFileAudio.getOriginalFilename();
-                            Path name = Paths.get(nom);
-                            if(!Files.exists(name)){
-                                Files.copy(multipartFileAudio.getInputStream(),
-                                        rootlocation.resolve(multipartFileAudio.getOriginalFilename()));
-                                guide.setAudio("http://localhost/audio_demarche/"
-                                        +multipartFileAudio.getOriginalFilename());
-                            }else{
-                                Files.delete(name);
-                                Files.copy(multipartFileAudio.getInputStream(),rootlocation.resolve(multipartFileAudio.getOriginalFilename()));
-                                guide.setAudio("http://localhost/audio_demarche/"
-                                        +multipartFileAudio.getOriginalFilename());
+                //ajout de l'audio
+                if (multipartFileAudio != null) {
+                    String location = "C:\\xampp\\htdocs\\audio_demarche";
+                    try {
+                        Path rootlocation = Paths.get(location);
+                        if (!Files.exists(rootlocation)) {
+                            Files.createDirectories(rootlocation);
+                            Files.copy(multipartFileAudio.getInputStream(),
+                                    rootlocation.resolve(multipartFileAudio.getOriginalFilename()));
+                            guide.setAudio("http://localhost/audio_demarche/"
+                                    + multipartFileAudio.getOriginalFilename());
+                        } else {
+                            try {
+                                String nom = location + "\\" + multipartFileAudio.getOriginalFilename();
+                                Path name = Paths.get(nom);
+                                if (!Files.exists(name)) {
+                                    Files.copy(multipartFileAudio.getInputStream(),
+                                            rootlocation.resolve(multipartFileAudio.getOriginalFilename()));
+                                    guide.setAudio("http://localhost/audio_demarche/"
+                                            + multipartFileAudio.getOriginalFilename());
+                                } else {
+                                    Files.delete(name);
+                                    Files.copy(multipartFileAudio.getInputStream(), rootlocation.resolve(multipartFileAudio.getOriginalFilename()));
+                                    guide.setAudio("http://localhost/audio_demarche/"
+                                            + multipartFileAudio.getOriginalFilename());
+                                }
+                            } catch (Exception e) {
+                                throw new Exception("Impossible de télécharger l\'audio");
                             }
-                        }catch (Exception e){
-                            throw new Exception("Impossible de télécharger l\'audio");
                         }
+                    } catch (Exception e) {
+                        throw new Exception(e.getMessage());
                     }
-                } catch (Exception e){
-                    throw new Exception(e.getMessage());
                 }
+                return guideRepository.save(guide);
+            } else {
+                throw new EntityExistsException("Ce guide existe déjà");
             }
-            return  guideRepository.save(guide);
-        }else{
-            throw new EntityExistsException("Ce guide existe déjà");
         }
     }
     //methode update
